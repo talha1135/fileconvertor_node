68d9a2ec2fddb3648ca31da671b08dd70ed8e9f4
talha1135 <92589533+talhabackenddev@users.noreply.github.com>
2023-01-27 17:08:35 +0500

    auto donwload

--- filedownload.js
+++ filedownload.js
@@ -1,19 +1,45 @@
-const https = require("https");
-const fs = require("fs");
-this.ping= function(latest){
+const {GoogleAuth} = require('google-auth-library');
+const {google} = require('googleapis');
+const {Storage} = require('@google-cloud/storage');
+ const projectId = 'rugged-diagram-375519';
+ async function authenticateImplicitWithAdc() {
+   // This snippet demonstrates how to list buckets.
+   // NOTE: Replace the client created below with the client required for your application.
+   // Note that the credentials are not specified when constructing the client.
+   // The client library finds your credentials using ADC.
+   const storage = new Storage({
+     projectId,
+   });
+   const [buckets] = await storage.getBuckets();
+   console.log('Buckets:');
+ 
+   for (const bucket of buckets) {
+     console.log(`- ${bucket.name}`);
+   }
+ 
+   console.log('Listed all storage buckets.');
+ }
+ 
+ authenticateImplicitWithAdc();
+// function(latest){
+   this.ping=  async function downloadFile(latest) {
 // URL of the image
-const url = latest.webViewLink;
 
-https.get(url, (res) => {
-   const path = "./files/"+latest.name;
-   const writeStream = fs.createWriteStream(path);
 
-   res.pipe(writeStream);
-
-   writeStream.on("finish", () => {
-      writeStream.close();
-      var filedownload = require("./index.js");
-      output = filedownload.myfunction(latest.name);
-   })
-})
+const auth = new GoogleAuth({
+   scopes: 'https://www.googleapis.com/auth/drive',
+ });
+ const service = google.drive({version: 'v3', auth});
+ fileId = latest.id;
+ try {
+   const file = await service.files.get({
+     fileId: fileId,
+     alt: 'media',
+   });
+   console.log(file.status);
+   return file.status;
+ } catch (err) {
+   // TODO(developer) - Handle error
+   throw err;
+ }
 }
